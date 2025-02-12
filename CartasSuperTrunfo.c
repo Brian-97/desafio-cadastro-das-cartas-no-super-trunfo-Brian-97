a8b00822e1d0b8d4b90266f77f50a4edcda9b3f9
Brian Borges <brianstz@hotmail.com>
2025-02-12 21:52:43 +0000

    Finalização do nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,37 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
 
 
     int pontos_turisticos;
-    float populacao, area, pib;
+    float populacao, area, pib, densidade, pib_capita;
     char cidade[50], cod_cidade[15];
 
     printf("Informe o nome da cidade \n");
-    scanf(cidade);
+    scanf("%49s", cidade);
 
-    print("Cidade: %cidade");
+    printf("Informe o codigo cidade \n");
+    scanf("%14s", cod_cidade);
+
+    printf("Informe a quantidade de pontos turisticos da cidade \n");
+    scanf("%d", &pontos_turisticos);
+
+    printf("Informe área da cidade \n");
+    scanf("%f", &area);
+
+    printf("Informe o PIB cidade \n");
+    scanf("%f", &pib);
+
+    printf("Informe a população da cidade \n");
+    scanf("%f", &populacao);
+
+    densidade = populacao / area;
+    pib_capita = pib / populacao;
+
+    printf("\t **** Carta configurada **** \n");
+    printf("\t\tCidade: %s \n\t\tCodigo: %s \n\t\tPontos Turisticos: %d \n\t\tÁrea da cidade: %f \n" , cidade, cod_cidade, pontos_turisticos, area);
+    printf("\t\tPIB: %f \n\t\tPopulação: %f \n" , pib, populacao);
+    printf("\t\tDensidade: %f \n\t\tPIB per capita: %f \n\n" , densidade, pib_capita);
 
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
